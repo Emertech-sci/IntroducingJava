ea37ebdcfe7abda34992c59b0623d5591beabfc5
Mateo Rendon MacBook <rendoncode@gmail.com>
2024-09-10 08:26:30 -0600

    firts twoo lessos

---
+++ IntroToJava/src/Tree.java
@@ -0,0 +1,30 @@
+/*
+Introduce yourself to users and plant a tree for them!
+ */
+//1- We’re starting with a blank Java file named Tree.java.
+//Define a class that follows the Java naming conventions.
+public class Tree {
+    //This code produces an error because our program needs a main() method.
+    //Define this method inside the curly braces of the Tree class.
+    public static void main(String[] args) {
+        //Write a comment in main() that describes the task it will perform.
+        //Below your comment, print a message introducing yourself to the user.
+        //After introducing yourself, use another print statement to output the project goal!
+        System.out.println("Hello my name is Mateo");
+        System.out.println("Today we are going to plant a tree in Java");
+        //We’ll use multiple print statements to plant our tree.
+        System.out.println("     *     ");
+        System.out.println("    ***    ");
+        System.out.println("   *****   ");
+        System.out.println("  *******  ");
+        System.out.println(" ********* ");
+        System.out.println("***********");
+        System.out.println("     *     ");
+        System.out.println("     *     ");
+        System.out.println("     *     ");
+
+
+
+
+    }
+}
